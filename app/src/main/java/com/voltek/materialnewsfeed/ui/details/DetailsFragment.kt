7c94e5c54d9a85268545e9100e5f94a26cace504
Ivan Antsiferov <voltek.dev@gmail.com>
2017-04-02 13:39:03 +0300

    Add fragment details layout.

--- app/src/main/java/com/voltek/materialnewsfeed/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/voltek/materialnewsfeed/ui/details/DetailsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.voltek.materialnewsfeed.R
 import com.voltek.materialnewsfeed.ui.BaseFragment
 
 class DetailsFragment : BaseFragment<DetailsContract.View, DetailsContract.Presenter>(),
@@ -20,7 +21,7 @@ class DetailsFragment : BaseFragment<DetailsContract.View, DetailsContract.Prese
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(null, container, false);
+        return inflater?.inflate(R.layout.fragment_details, container, false);
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
