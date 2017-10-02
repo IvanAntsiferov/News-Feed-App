32459f5ed17852116faffffdaf04ee3b25a5c337
Antsiferov Ivan <antsiferov.ivan@gmail.com>
2017-10-02 05:56:06 +0300

    Add test for all navigation commands.

--- app/src/test/java/com/voltek/newsfeed/presentation/navigation/RouterHolderTest.kt
+++ app/src/test/java/com/voltek/newsfeed/presentation/navigation/RouterHolderTest.kt
@@ -1,5 +1,7 @@
 package com.voltek.newsfeed.presentation.navigation
 
+import com.voltek.newsfeed.domain.exception.NoConnectionException
+import com.voltek.newsfeed.presentation.entity.ArticleUI
 import com.voltek.newsfeed.presentation.navigation.command.*
 import com.voltek.newsfeed.presentation.navigation.proxy.Command
 import com.voltek.newsfeed.presentation.navigation.proxy.Navigator
@@ -8,6 +10,7 @@ import com.voltek.newsfeed.presentation.navigation.proxy.Router
 import org.junit.Before
 import org.junit.Test
 import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
 
 class RouterHolderTest {
 
@@ -81,5 +84,28 @@ class RouterHolderTest {
         navigatorBinder.removeNavigator()
         navigatorBinder.setNavigator(trueNavigator)
         assertEquals(0, routerHolder.commandsQueue.size)
+    }
+
+    @Test
+    fun allCommandsTest() {
+        val command = CommandSystemMessage("Error", NoConnectionException())
+        router.execute(command)
+        assertTrue(routerHolder.commandsQueue[0] is CommandSystemMessage)
+        val realCommand = routerHolder.commandsQueue[0] as CommandSystemMessage
+        assertEquals(command.message, realCommand.message)
+        assertEquals(command.error, realCommand.error)
+        router.execute(CommandBack())
+        assertEquals(2, routerHolder.commandsQueue.size)
+        router.execute(CommandOpenWebsite("url"))
+        assertEquals("url", (routerHolder.commandsQueue[2] as CommandOpenWebsite).url)
+        router.execute(CommandShareArticle("title", "url"))
+        assertEquals(4, routerHolder.commandsQueue.size)
+        assertEquals("title", (routerHolder.commandsQueue[3] as CommandShareArticle).title)
+        assertEquals("url", (routerHolder.commandsQueue[3] as CommandShareArticle).url)
+        router.execute(CommandOpenArticleDetailsScreen(ArticleUI()))
+        assertTrue((routerHolder.commandsQueue[4] as CommandOpenArticleDetailsScreen).article.isEmpty())
+        assertEquals(5, routerHolder.commandsQueue.size)
+        navigatorBinder.setNavigator(trueNavigator)
+        assertTrue(routerHolder.commandsQueue.isEmpty())
     }
 }
